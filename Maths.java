1624e365586bcd4f21c7dd39740c6136e23897a9
Arina <32911890+arinabarnett@users.noreply.github.com>
2017-11-01 22:13:59 +0300

    Update Maths.java

--- Maths.java
+++ Maths.java
@@ -1,23 +1,29 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.Scanner; 
+import java.util.Scanner;
 
-public class Main {
+public class Maths {
 
-  public static void main(String[] args) throws IOException {
-    int a;
-    int b;
-    int c;
-    Scanner reader = new Scanner(System.in);
-    System.out.println("Enter your number: ");
-    a = reader.nextInt();
-    System.out.println("Enter the second number: ");
-    b = reader.nextInt();
-    System.out.println("Enter the last number: ");
-    c = reader.nextInt();
-    System.out.println("This is the answer: ");
-    System.out.println(a+b+c);
-    
-    reader.close();
-  }
+    public static void main(String[] args) throws IOException {
+        private int sum, sub, mult, numberA, numberB;
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        numberA = reader.nextInt();
+
+        System.out.println("Enter the second number: ");
+        numberB = reader.nextInt();
+
+        sum = numberA + numberB;
+        System.out.println("The sum of the two numbers you entered = " + sum);
+
+        sub = numberA - numberB;
+        System.out.println("The subtraction of the two numbers you entered = " + sub);
+
+        mult = numberA * numberB;
+        System.out.println("The multiplication of the two numbers you entered = " + mult);
+
+        reader.close();
+    }
+}
