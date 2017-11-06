ecf5c89cd8cf898957c204c0780dfec5518bff96
Arina <32911890+arinabarnett@users.noreply.github.com>
2017-11-06 18:14:25 +0300

    Update Shape.java
    Реализовать иерархию классов для геометрических фигур, вычислить их площадь.

--- Shape.java
+++ Shape.java
@@ -1,1 +1,31 @@
 
+public class Figure {
+    public static void main (String args[]);
+    int radius, side1, side2, side3;
+    double area;
+}
+
+abstract class Circle extends Figure {
+    public abstract void main (String args[]) {
+        int radius = 6;
+        double area = Math.PI * ( radius * radius );
+        System.out.println("Площадь круга равна: " + area );
+    }
+}
+
+abstract class RightTraingle extends Figure {
+    public abstract void main (String args[]) {
+        int side1 = 2;
+        int side2 = 4;
+        double area = ( side1 * side2 / 2 );
+        System.out.println("Площадь прямоугольного треугольника равна: " + area );
+    }
+}
+
+abstract class Square extends Figure {
+    public abstract void main (String args[]) {
+    int side3 = 5;
+    double area = (side3 * 2);
+    System.out.println("Площадь квадрата: " + area);
+    }
+}
