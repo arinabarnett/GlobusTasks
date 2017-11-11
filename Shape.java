10d2794e159dc40b059bac4734ae7de37017b2e7
Arina <32911890+arinabarnett@users.noreply.github.com>
2017-11-11 13:34:58 +0300

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,20 +1,23 @@
 
-public class Figure {
-    public static void main (String args[]);
-    int radius, side1, side2, side3;
-    double area;
+public class Shape {
+    public static void main(String args[]) {
+        int radius, side1, side2, side3;
+        double area;
+    }
 }
 
-abstract class Circle extends Figure {
-    public abstract void main (String args[]) {
+abstract class Circle extends Shape {
+    public static void area()
+    {
         int radius = 6;
         double area = Math.PI * ( radius * radius );
         System.out.println("Площадь круга равна: " + area );
     }
 }
 
-abstract class RightTraingle extends Figure {
-    public abstract void main (String args[]) {
+abstract class RightTraingle extends Shape {
+    public static void area()
+    {
         int side1 = 2;
         int side2 = 4;
         double area = ( side1 * side2 / 2 );
@@ -22,10 +25,11 @@ abstract class RightTraingle extends Figure {
     }
 }
 
-abstract class Square extends Figure {
-    public abstract void main (String args[]) {
-    int side3 = 5;
-    double area = (side3 * 2);
-    System.out.println("Площадь квадрата: " + area);
+abstract class Square extends Shape {
+    public static void area()
+    {
+        int side3 = 5;
+        double area = (side3 * 2);
+        System.out.println("Площадь квадрата: " + area);
     }
 }
