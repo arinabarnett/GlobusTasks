913e5b514c1f0f1d89a771918118715fff76dfc7
Arina <32911890+arinabarnett@users.noreply.github.com>
2017-11-13 15:04:38 +0300

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,1 +1,26 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
+public class FileReader {
+    public static void main(String[] args) {
+        File file = new File("C:\NewTextFile.txt");
+        if (!file.exists()) {
+            System.out.println(args[0] + " does not exist.");
+            return;
+        }
+        if (!(file.isFile() && file.canRead())) {
+            System.out.println(file.getName() + " cannot be read from.");
+            return;
+        }
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            char current;
+            while (fis.available() > 0) {
+                current = (char) fis.read();
+                System.out.print(current);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
